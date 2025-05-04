3ce50be99279eb565767e7de8aa7475a23dd4f6b
sefler <sefler@126.com>
2025-05-04 12:32:18 +0800

    change to synchronous draw

--- bVNC/src/main/java/com/qihua/bVNC/FpsCounter.java
+++ bVNC/src/main/java/com/qihua/bVNC/FpsCounter.java
@@ -17,7 +17,6 @@ public class FpsCounter {
     private long maxlatency = 0;
     private long avglatency = 0;
     private long maxlastMs = 0;
-    private int qsize = 0;
     private int frameDropped = 0;
     private long frameDroppedMs = 0;
 
@@ -37,7 +36,7 @@ public class FpsCounter {
         fps += 1;
     }
 
-    public void finish(long inFpsMs, int qsize) {
+    public void finish(long inFpsMs) {
         long latency = System.currentTimeMillis() - inFpsMs;
         avglatency = (avglatency + latency)/ 2;
         if (latency > maxlatency) {
@@ -53,19 +52,14 @@ public class FpsCounter {
             frameDropped = 0;
             frameDroppedMs = System.currentTimeMillis();
         }
-
-        this.qsize  = qsize;
     }
 
     public void drawFps(Canvas canvas) {
         char[] text = ("FPS:" + lst + ", AVG:" + avg).toCharArray();
         canvas.drawText(text, 0, text.length, 100f, 100f, _textPaint);
 
-        char[] latText = ("DELAY: MAX-5=" + maxlatency + ", AVG=" + avglatency).toCharArray();
+        char[] latText = ("DRAW COST: MAX-5=" + maxlatency + ", AVG=" + avglatency).toCharArray();
         canvas.drawText(latText, 0, latText.length, 100f, 140f, _textPaint);
-
-        char[] qText = ("Q-SIZE:" + qsize + ", DROP-10:" + frameDropped).toCharArray();
-        canvas.drawText(qText, 0, qText.length, 100f, 180f, _textPaint);
 
         reset();
     }
