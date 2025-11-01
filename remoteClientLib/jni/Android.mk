0c2f57eded936d4531bc6ea31c97ff35aaee2487
sefler <sefler@126.com>
2025-11-01 13:48:31 +0800

    try to enable support for 16KB memory

--- remoteClientLib/jni/Android.mk
+++ remoteClientLib/jni/Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH 	:= $(call my-dir)
 COMMON_ROOT	:= libs/deps/$(TARGET_ARCH_ABI)
 PREBUILT_ROOT   := $(COMMON_ROOT)/root
 GSTREAMER_ROOT  := $(COMMON_ROOT)/gstreamer
-LOCAL_LDFLAGS += "-Wl,-z,max-page-size=16384"
+LOCAL_LDFLAGS += -Wl,-z,max-page-size=16384
 
 include $(call all-subdir-makefiles)
