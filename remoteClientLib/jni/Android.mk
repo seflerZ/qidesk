fdf9d35461e8e830f87b92e31165628e31589117
sefler <sefler@126.com>
2025-11-04 22:42:01 +0800

    updated to ndk 27d

--- remoteClientLib/jni/Android.mk
+++ remoteClientLib/jni/Android.mk
@@ -4,5 +4,6 @@ PREBUILT_ROOT   := $(COMMON_ROOT)/root
 GSTREAMER_ROOT  := $(COMMON_ROOT)/gstreamer
 LOCAL_LDFLAGS += -Wl,-z,max-page-size=16384
 LOCAL_CFLAGS += -mllvm -page-size=16384
+APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
 
 include $(call all-subdir-makefiles)
