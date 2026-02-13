d36004a5f62e2c4d253bcc3be85eaf624c19380a
sefler <sefler@126.com>
2026-02-13 14:13:05 +0800

    feature gate and change app name to QiDesk

--- bVNC/src/main/java/com/qihua/bVNC/GlobalPreferencesFragment.java
+++ bVNC/src/main/java/com/qihua/bVNC/GlobalPreferencesFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.qihua.bVNC.extrakeys.ExtraKeysConstants;
 
@@ -13,15 +14,10 @@ public class GlobalPreferencesFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle bundle, String s) {
         getPreferenceManager().setSharedPreferencesName(Constants.generalSettingsTag);
         setPreferencesFromResource(R.xml.global_preferences, s);
-        if (Utils.isVnc(getContext())) {
-            addPreferencesFromResource(R.xml.global_preferences_vnc);
-        } else if (Utils.isRdp(getContext())) {
-            addPreferencesFromResource(R.xml.global_preferences_exfrakeys);
-            addPreferencesFromResource(R.xml.global_preferences_touchpad);
-//            addPreferencesFromResource(R.xml.global_preferences_rdp);
-        } else if (Utils.isSpice(getContext())) {
-            addPreferencesFromResource(R.xml.global_preferences_spice);
-        }
+
+        addPreferencesFromResource(R.xml.global_preferences_exfrakeys);
+        addPreferencesFromResource(R.xml.global_preferences_touchpad);
+//      addPreferencesFromResource(R.xml.global_preferences_rdp);
 
         EditTextPreference horizontalExtraKeysPref = findPreference(Constants.horizontalExtraKeysPref);
         if (horizontalExtraKeysPref != null && "".equals(horizontalExtraKeysPref.getText())) {
@@ -34,7 +30,7 @@ public class GlobalPreferencesFragment extends PreferenceFragmentCompat {
         }
 
         // Disable edge wheel preference in free version
-        SwitchPreference edgeWheelPref = findPreference(Constants.touchpadEdgeWheel);
+        SwitchPreferenceCompat edgeWheelPref = findPreference(Constants.touchpadEdgeWheel);
         if (edgeWheelPref != null) {
             edgeWheelPref.setEnabled(BuildConfig.EDGE_ENABLED);
         }
